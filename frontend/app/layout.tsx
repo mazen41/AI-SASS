378b71be510820cb49d0aecbaa94565fbfbf8989
Mazen Hossny <mazehossny@gmail.com>
2026-05-26 00:29:48 +0300

    Message

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ const nunito = Nunito({
 
 const baloo = Baloo_2({
   subsets: ['latin'],
-  weight: ['400', '600', '700', '800', '900'],
+  weight: ['400', '500', '600', '700', '800'],
   variable: '--font-baloo',
 });
 
